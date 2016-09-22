816d4a501681467cca1b774bbae2450cf43e6867
Malavika Srinivasan <malavikasrinivasan@Malavikas-MacBook-Pro.local>
2016-09-22 09:19:02 -0700

    HW3 solutions

--- Assignment/HW3-JS_ToDo_List/_js/main.js
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -1,13 +1,30 @@
 $(document).ready(
-    $("#new-item").on('click', function() {
-        // once the document loads, create new item with this function
+    $("#add-item").on('click', function() {
+ 
+        var todo =  $("#to-do-input").val(); // Get value from the form
+        var move_to_done = '<span class="glyphicon glyphicon-triangle-right" id="completed-icon" aria-hidden="true"></span>';
+        if(todo.trim() != "") {
+        	$("#to-dos").prepend('<li>' + todo + move_to_done + '</li>'); // Add to the to-do list
+        }
+        $("#to-do-input").val(""); // Clear the form
     })
 );
 
-$("#list_todo").on('click', "button", function() {
-        // move from list_todo container to list_completed container
+$("#to-dos").on('click', "#completed-icon", function() {
+        // move from to do to done
+        var done = $(this).parent().not('span').text().trim(); // Get only the text 
+        var move_to_do = '<span class="glyphicon glyphicon-triangle-left" id="incomplete-icon" aria-hidden="true"></span>';
+        var move_to_done = '<span class="glyphicon glyphicon-triangle-right" id="completed-icon" aria-hidden="true"></span>';
+        $("#dones").prepend('<li>' + move_to_do + done + move_to_done +'</li>');
+        $(this).parent().hide();
+
 });
 
-$("#list_completed").on('click', "button", function() {
-        // move back from list_completed container to list_todo container
+$("#dones").on('click', "#incomplete-icon", function() {
+        // move back from done to to do
+        var todo = $(this).parent().not('span').text();
+        var move_to_done = '<span class="glyphicon glyphicon-triangle-right" id="completed-icon" aria-hidden="true"></span>';
+        $("#to-dos").prepend('<li>' + todo + move_to_done + '</li>'); // Add to the to-do list
+        $(this).parent().hide();
+
 });
